826d2e14e7f02eab4a8dc066042d52da958177c2
SebastianTovar64 <sebastovaes@hotmail.com>
2023-09-18 15:09:51 -0500

    Java
    Proyecto

---
+++ ProyectoJavaScript/js/holaMundo.js
@@ -0,0 +1,19 @@
+nacimiento=parseInt(prompt("Ingresa tu año de Nacimiento"));
+const fecha = new Date();
+const fechaActual=fecha.getFullYear();
+let edad = fechaActual-nacimiento;
+var nombres= prompt("Ingresa tu Nombre");
+//var Edad= parseInt(prompt("Ingresa tu Edad"));//
+//document.write("Bienvenidos señor(a) "+nombres+" usted tiene "+edad+" años de edad")//
+
+if (edad<=8){
+    document.write("Bienvenidos niño(a) "+nombres+" usted tiene "+edad+" años de edad")
+}
+else 
+
+if(edad<=17){
+    document.write("Bienvenidos joven "+nombres+" usted tiene "+edad+" años de edad")
+}
+else{
+    document.write("Bienvenidos señor(a) "+nombres+" usted tiene "+edad+" años de edad")
+}
