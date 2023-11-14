8bdbd7527eb10fd49e4f478378d4ece7660dfad1
SebastianTovar64 <129071813+SebastianTovar64@users.noreply.github.com>
2023-11-14 11:46:32 -0500

    JS
    JS

--- ProyectoJavaScript2.2/js/calculadoraMejorada.js
+++ ProyectoJavaScript2.2/js/calculadoraMejorada.js
@@ -1,98 +1,135 @@
-function sumar(n1,n2){
-    var resultado=n1+n2;
-    var res=document.getElementById("resultado")
-    if (n1!=0 && n2!=0){
-        respuesta.style.display="block";
-        res.innerHTML = `Total de la suma: ${resultado}`;
-        res.style.color="black solid";
-    }else{
-        respuesta.style.display="block";
-        res.innerHTML = `verifique sus datos`;
-        res.style.color="red";
-    }
+// Funcion para sumar dos numeros
+function sumar(n1, n2) {
+    var resultado = n1 + n2;
+    var res = document.getElementById("resultado");
 
+    // Verifica que los numeros ingresados no sean cero
+    if (n1 != 0 && n2 != 0) {
+        // Si no son cero, muestra el resultado de la suma en el elemento HTML "resultado"
+        respuesta.style.display = "block";
+        res.innerHTML = `Total de la suma: ${resultado}`;
+        res.style.color = "black solid";
+    } else {
+        // Si algun numero es cero, muestra un mensaje de error en rojo
+        respuesta.style.display = "block";
+        res.innerHTML = `Verifique sus datos`;
+        res.style.color = "red";
+    }
 }
-function restar(){
-    let n1=parseInt (document.getElementById("n1"). value)
-    let n2=parseInt (document. getElementById("n2"). value)
-    resultado=n1-n2;
-    var res=document.getElementById("resultado")
-    if (n1!=0 && n2!=0){
-        respuesta.style.display="block";
+
+// Funcion para restar dos numeros
+function restar() {
+    // Obtiene los valores de los elementos HTML con id "n1" y "n2"
+    let n1 = parseInt(document.getElementById("n1").value);
+    let n2 = parseInt(document.getElementById("n2").value);
+    resultado = n1 - n2;
+    var res = document.getElementById("resultado");
+
+    // Verifica que los numeros ingresados no sean cero
+    if (n1 != 0 && n2 != 0) {
+        // Si no son cero, muestra el resultado de la resta en el elemento HTML "resultado"
+        respuesta.style.display = "block";
         res.innerHTML = `Total de la resta : ${resultado}`;
-        res.style.color="black solid";
-    }else{
-        respuesta.style.display="block";
-        res.innerHTML = `verifique sus datos`;
-        res.style.color="red";
+        res.style.color = "black solid";
+    } else {
+        // Si algun numero es cero, muestra un mensaje de error en rojo
+        respuesta.style.display = "block";
+        res.innerHTML = `Verifique sus datos`;
+        res.style.color = "red";
     }
-    }
-    function multiplicar(){
-    let n1=parseInt (document.getElementById("n1").value)
-    let n2=parseInt (document.getElementById("n2").value)
-    resultado=n1*n2;
-    var res=document.getElementById("resultado")
-    if (n1!=0 && n2!=0){
-        respuesta.style.display="block";
+}
+
+// Funcion para multiplicar dos numeros
+function multiplicar() {
+    // Obtiene los valores de los elementos HTML con id "n1" y "n2"
+    let n1 = parseInt(document.getElementById("n1").value);
+    let n2 = parseInt(document.getElementById("n2").value);
+    resultado = n1 * n2;
+    var res = document.getElementById("resultado");
+
+    // Verifica que los numeros ingresados no sean cero
+    if (n1 != 0 && n2 != 0) {
+        // Si no son cero, muestra el resultado de la multiplicacion en el elemento HTML "resultado"
+        respuesta.style.display = "block";
         res.innerHTML = `Total de la multiplicacion : ${resultado}`;
-        res.style.color="black solid";
-    }else{
-        respuesta.style.display="block";
-        res.innerHTML = `verifique sus datos`;
-        res.style.color="red";
+        res.style.color = "black solid";
+    } else {
+        // Si algun numero es cero, muestra un mensaje de error en rojo
+        respuesta.style.display = "block";
+        res.innerHTML = `Verifique sus datos`;
+        res.style.color = "red";
     }
-    }
-    function dividir0(){
-    let n1=parseInt (document. getElementById ("n1"). value)
-    let n2=parseInt (document.getElementById("n2"). value)
-    resultado=n1/n2;
-    var res=document.getElementById("resultado")
-    if (n1!=0 && n2!=0){
-        respuesta.style.display="block";
+}
+
+// Funcion para dividir dos numeros
+function dividir0() {
+    // Obtiene los valores de los elementos HTML con id "n1" y "n2"
+    let n1 = parseInt(document.getElementById("n1").value);
+    let n2 = parseInt(document.getElementById("n2").value);
+    resultado = n1 / n2;
+    var res = document.getElementById("resultado");
+
+    // Verifica que los numeros ingresados no sean cero
+    if (n1 != 0 && n2 != 0) {
+        // Si no son cero, muestra el resultado de la division en el elemento HTML "resultado"
+        respuesta.style.display = "block";
         res.innerHTML = `Total de la division : ${resultado}`;
-        res.style.color="black solid";
-    }else{
-        respuesta.style.display="block";
-        res.innerHTML = `verifique sus datos`;
-        res.style.color="red";
+        res.style.color = "black solid";
+    } else {
+        // Si algun numero es cero, muestra un mensaje de error en rojo
+        respuesta.style.display = "block";
+        res.innerHTML = `Verifique sus datos`;
+        res.style.color = "red";
     }
-    }
-    function potencia(){
-    let n1=parseInt(document.getElementById("n1").value)
-    resultado=n1*n1;
-    var res=document.getElementById("resultado")
-    if (n1!=0 && n2!=0){
-        respuesta.style.display="block";
+}
+
+// Funcion para calcular la potencia cuadrada de un numero
+function potencia() {
+    // Obtiene el valor del elemento HTML con id "n1"
+    let n1 = parseInt(document.getElementById("n1").value);
+    resultado = n1 * n1;
+    var res = document.getElementById("resultado");
+
+    // Verifica que el numero ingresado no sea cero
+    if (n1 != 0) {
+        // Si no es cero, muestra el resultado de la potencia en el elemento HTML "resultado"
+        respuesta.style.display = "block";
         res.innerHTML = `Total de la potencia : ${resultado}`;
-        res.style.color="black solid";
-    }else{
-        respuesta.style.display="block";
-        res.innerHTML = `verifique sus datos`;
-        res.style.color="red";
+        res.style.color = "black solid";
+    } else {
+        // Si el numero es cero, muestra un mensaje de error en rojo
+        respuesta.style.display = "block";
+        res.innerHTML = `Verifique sus datos`;
+        res.style.color = "red";
     }
+}
+
+// Funcion principal que determina la operacion a realizar segun la opcion seleccionada
+function mostrar() {
+    // Obtiene los valores de los elementos HTML con id "n1", "n2" y "opc"
+    let n1 = parseInt(document.getElementById("n1").value);
+    let n2 = parseInt(document.getElementById("n2").value);
+    let opc = parseInt(document.getElementById("opc").value);
+
+    // Utiliza un switch para determinar que operacion realizar
+    switch (opc) {
+        case 1:
+            sumar(n1, n2);
+            break;
+        case 2:
+            restar();
+            break;
+        case 3:
+            multiplicar();
+            break;
+        case 4:
+            dividir0();
+            break;
+        case 5:
+            potencia();
+            break;
+        default:
+            // Muestra una alerta si la opcion ingresada no es valida
+            alert("Opcion no valida");
     }
-    function mostrar(){
-        //opc=parseInt (prompt ("ingresa opcion 1 para sumar 2 para restar, 3 multiplicar,4 d
-        let n1=parseInt (document.getElementById("n1").value)
-        let n2=parseInt (document.getElementById("n2") .value)
-        let opc=parseInt (document. getElementById("opc") .value)
-        switch (opc){
-            case 1:
-                sumar(n1,n2)
-                break;
-            case 2:
-                restar()
-                break;
-            case 3:
-                multiplicar()
-                break;
-            case 4:
-                dividir0()
-                break;
-            case 5: 
-                potencia()
-                break;
-            default:
-                alert("opcion no valida");
-        }
-    }
+}
