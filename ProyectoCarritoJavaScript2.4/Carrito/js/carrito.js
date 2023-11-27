f8a7d37d5677ac2e66013be703a9f8b5ab25037d
SebastianTovar64 <129071813+SebastianTovar64@users.noreply.github.com>
2023-11-27 16:36:27 -0500

    ProyectoJavaScript
    JavaScript

--- ProyectoCarritoJavaScript2.4/Carrito/js/carrito.js
+++ ProyectoCarritoJavaScript2.4/Carrito/js/carrito.js
@@ -1,71 +1,65 @@
-let carrito = [];
+let carrito = []; // Arreglo para almacenar los productos en el carrito
 
 window.addEventListener('keydown', (e) => {
     if (e.key === "+") {
-        incremento();
+        incremento(); // Llama a la funcion incremento() si se presiona la tecla "+"
     } else if (e.key === "-") {
-        decremento();
+        decremento(); // Llama a la funcion decremento() si se presiona la tecla "-"
     }
 });
 
-const valor = document.getElementById("valor");
+const valor = document.getElementById("valor"); // Obtiene el elemento con el id "valor" del documento HTML
 
 function actualizarValor() {
-    // Crear un string para almacenar la presentación de los productos
-    let productosHTML = "";
+    let productosHTML = ""; // String para almacenar la presentacion de los productos
 
-    // Calcular el total y presentar cada producto
-    let total = 0;
+    let total = 0; // Variable para almacenar el valor total de los productos en el carrito
     carrito.forEach((producto) => {
-        total += producto.valor * producto.cantidad;
-        productosHTML += `<p>${producto.nombre} x${producto.cantidad}: ${producto.valor * producto.cantidad} </p>`;
+        total += producto.valor * producto.cantidad; // Calcula el valor total sumando el valor del producto multiplicado por la cantidad
+        productosHTML += `<p>${producto.nombre} x${producto.cantidad}: ${producto.valor * producto.cantidad} </p>`; // Agrega la presentacion de cada producto al string productosHTML
     });
 
-    // Mostrar la cantidad y el total de cada producto
-    valor.innerHTML = `<div>${productosHTML}</div><p>Total: ${total}</p>`;
+    valor.innerHTML = `<div>${productosHTML}</div><p>Total: ${total}</p>`; // Muestra la cantidad y el total de cada producto en el documento HTML
 }
 
 function incremento() {
-    // Buscar si el producto ya está en el carrito
-    const producto1 = { nombre: "Hamburguesa", valor: 20000, cantidad: 1 };
-    const producto2 = { nombre: "Cotel", valor: 60000, cantidad: 1 };
+    const producto1 = { nombre: "El Coronel No Tiene Quien Le Escriba", valor: 200000, cantidad: 1 }; // Producto 1
+    const producto2 = { nombre: "100 Años De Soledad", valor: 600000, cantidad: 1 }; // Producto 2
 
-    const indexProducto1 = carrito.findIndex((p) => p.nombre === producto1.nombre);
-    const indexProducto2 = carrito.findIndex((p) => p.nombre === producto2.nombre);
+    const indexProducto1 = carrito.findIndex((p) => p.nombre === producto1.nombre); // Busca si el producto1 ya esta en el carrito
+    const indexProducto2 = carrito.findIndex((p) => p.nombre === producto2.nombre); // Busca si el producto2 ya esta en el carrito
 
-    // Incrementar la cantidad si el producto ya está en el carrito, de lo contrario, agregarlo
-    if (indexProducto1 !== -1) {
-        carrito[indexProducto1].cantidad++;
+    if (indexProducto1 !== -1) { // Si el producto1 ya esta en el carrito
+        carrito[indexProducto1].cantidad++; // Incrementa la cantidad del producto1 en el carrito
     } else {
-        carrito.push(producto1);
+        carrito.push(producto1); // Agrega el producto1 al carrito
     }
 
-    if (indexProducto2 !== -1) {
-        carrito[indexProducto2].cantidad++;
+    if (indexProducto2 !== -1) { // Si el producto2 ya está en el carrito
+        carrito[indexProducto2].cantidad++; // Incrementa la cantidad del producto2 en el carrito
     } else {
-        carrito.push(producto2);
+        carrito.push(producto2); // Agrega el producto2 al carrito
     }
 
-    actualizarValor();
+    actualizarValor(); // Actualiza y muestra la cantidad y el valor total de los productos en el carrito
 }
 
 function decremento() {
-    // Quitar un producto del carrito
-    if (carrito.length > 0) {
-        const ultimoProducto = carrito[carrito.length - 1];
-        if (ultimoProducto.cantidad > 1) {
-            ultimoProducto.cantidad--;
+    if (carrito.length > 0) { // Si hay productos en el carrito
+        const ultimoProducto = carrito[carrito.length - 1]; // Obtiene el ultimo producto agregado al carrito
+
+        if (ultimoProducto.cantidad > 1) { // Si la cantidad del ultimo producto es mayor a 1
+            ultimoProducto.cantidad--; // Decrementa la cantidad del último producto en el carrito
         } else {
-            carrito.pop();
+            carrito.pop(); // Quita el ultimo producto del carrito
         }
 
-        actualizarValor();
+        actualizarValor(); // Actualiza y muestra la cantidad y el valor total de los productos en el carrito
     }
 }
 
 function resetear() {
-    // Limpiar el carrito
-    carrito = [];
-    actualizarValor();
-}
+    carrito = []; // Limpia el carrito
 
+    actualizarValor(); // Actualiza y muestra la cantidad y el valor total de los productos en el carrito
+}
--- ProyectoContadorJavaScript2.4.1/js/contador.js
+++ ProyectoContadorJavaScript2.4.1/js/contador.js
@@ -1,31 +1,38 @@
-window.addEventListener('keydown',(e)=>{
-    if (e.key =="+"){
-        icremento()
-}
-else if (e.key =="-"){
-    descremento()
-}
-})
-let contador=0;
-const valor=document.getElementById("valor")
-
-function incremento(){
-    //alert("incremento")
-    contador+=1;
-    //localStorage.setItem("contador",contador)
-    valor.innerHTML=contador;
-}
-function decremento(){
-    //alert("incremento")
-    if (contador>0){
-        contador-=1;
-        valor.innerHTML=contador;
-    }else{
-        contador=0;
-        valor.innerHTML=contador;
+// Agrega un evento al escuchar las teclas presionadas en la ventana
+window.addEventListener('keydown', (e) => {
+    // Si la tecla presionada es "+", se llama a la función "incremento"
+    if (e.key === '+') {
+        incremento();
+    } 
+    // Si la tecla presionada es "-", se llama a la función "decremento"
+    else if (e.key === '-') {
+        decremento();
     }
-}
-function resetear(){
-    contador=0;
-    valor.innerHTML=contador;
-}
+    });
+  
+  // Establece el contador en 0 y obtiene el elemento HTML con el id "valor"
+    let contador = 0;
+    const valor = document.getElementById('valor');
+  
+  // Incrementa el contador en 1 y actualiza el valor mostrado en la página
+    function incremento() {
+    contador += 1;
+    valor.innerHTML = contador;
+    }
+  
+  // Disminuye el contador en 1 si es mayor que 0, de lo contrario, lo deja en 0 y actualiza el valor mostrado en la página
+    function decremento() {
+    if (contador > 0) {
+        contador -= 1;
+        valor.innerHTML = contador;
+    } else {
+        contador = 0;
+        valor.innerHTML = contador;
+    }
+    }
+  
+  // Establece el contador en 0 y actualiza el valor mostrado en la página
+    function resetear() {
+    contador = 0;
+    valor.innerHTML = contador;
+    }
